5e97827217504fb8c5a420df2b90fe737068bbe0
unknown <you@example.comomelchuk.8787@gmail.com>
2025-07-09 18:58:12 +0300

    all

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-function getElementWidth(content, padding, border) {
-    let contentWidts = parseFloat(content);
-    let paddingWidts = parseFloat(padding);
-    let borderWidts = parseFloat(border);
-    return contentWidts + paddingWidts * 2 + borderWidts * 2;
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
